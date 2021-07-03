3a0a4804f460fbd7cb1bdee6029bec4ab0d764b3
AzarinaKS <shakester8@yandex.ru>
2021-07-03 19:57:31 +0700

    массив и многомерный массив

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Tetris
 {
@@ -9,25 +10,41 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            var p1 = new Point
-            {
-                C = '*',
-                X = 3,
-                Y = 5
-            };
+            // ВАРИАНТ СЕРЕЖИ
+            //var p1 = new Point
+            //{
+            //    C = '*',
+            //    X = 3,
+            //    Y = 5
+            //};
 
-            var p2 = new Point
-            {
-                C = '*',
-                X = 6,
-                Y = 8
-            };
+            //var p2 = new Point
+            //{
+            //    C = '*',
+            //    X = 6,
+            //    Y = 8
+            //};
 
+            //p1.Draw();
+            //p2.Draw();
+
+            //Point p1 = new Point();
+            //p1.Draw();
+
+            // ВАРИАНТ ОТУС
+
+            Point p1 = new Point(2, 3, '*');
             p1.Draw();
+
+            Point p2 = new Point()
+            {
+                X = 4,
+                Y = 5,
+                C = '#',
+            };
             p2.Draw();
 
-            Console.ReadLine();
-
+                Console.ReadLine();
         }
     }
 }
