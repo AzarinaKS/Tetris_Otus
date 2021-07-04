3b0ede28e8632dea41224ec1abca40ee9685bc20
AzarinaKS <shakester8@yandex.ru>
2021-07-04 21:37:49 +0700

    rotate

--- Tetris/Tetris/Stick.cs
+++ Tetris/Tetris/Stick.cs
@@ -14,5 +14,34 @@ namespace Tetris
             points[3] = new Point(x, y + 3, sym);
         }
 
+        public override void Rotate()
+        {
+            if (points[0].X == points[1].X)
+            {
+                RotateHorisontal();
+            }
+            else
+            {
+                RotateVertical();
+            }
+        }
+
+        private void RotateVertical()
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].Y = points[0].Y+i;
+                points[i].X = points[0].X;
+            }
+        }
+
+        private void RotateHorisontal()
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].Y = points[0].Y;
+                points[i].X = points[0].X + i;
+            }
+        }
     }
 }
